2e16454fcc7ad0488058a37aa10e7fa179044eaf
GrigoriiZ <grisha.zaitcev@gmail.com>
2026-01-08 15:36:29 +0400

    Добавление структуры проекта

---
+++ src/com/course/syntax/topic01_first_program/First_program.java
@@ -0,0 +1,9 @@
+package com.course.syntax.topic01_first_program;
+
+public class First_program {
+    public static void main(String[] args) {
+        System.out.print("Я думаю");
+        System.out.print(", быть программистом");
+        System.out.print(" круто!");
+    }
+}
